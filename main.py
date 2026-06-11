fa7264c441f936de99da1096dd2cf3446a8b93cd
Tidiane Khan <tidianekhan@MacBook-Air.local>
2026-06-11 10:39:55 +0200

    Add real CoachTribe logo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from search import search_courses
 import os
@@ -15,6 +16,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class SearchRequest(BaseModel):
     query: str
